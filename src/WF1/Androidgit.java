e56483bfb4fb912b5e58c9886a52afc146b6dbc1
adityaginandanwar <adityav.nandanwar@gmaill.com>
2019-09-12 18:35:46 +0300

    new repo for branch

---
+++ src/WF1/Androidgit.java
@@ -0,0 +1,30 @@
+package WF1;
+
+import org.testng.annotations.Test;
+
+public class Androidgit {
+
+	@Test
+
+	public void EighthTest()
+
+	{
+		System.out.print("EighthTest");
+	}
+	
+	@Test
+
+	public void NinethTest()
+
+	{
+		System.out.print("NinethTest");
+	}
+	
+	@Test
+
+	public void TenthTest()
+
+	{
+		System.out.print("TenthTest");
+	}
+}
---
+++ src/WF1/Apigit.java
@@ -0,0 +1,31 @@
+package WF1;
+
+import org.testng.annotations.Test;
+
+public class Apigit {
+
+	
+	@Test
+
+	public void FifththTest()
+
+	{
+		System.out.print("FifthTest");
+	}
+	
+	@Test
+
+	public void SixthTest()
+
+	{
+		System.out.print("SixthTest");
+	}
+	
+	@Test
+
+	public void SeventhTest()
+
+	{
+		System.out.print("SeventhTest");
+	}
+}

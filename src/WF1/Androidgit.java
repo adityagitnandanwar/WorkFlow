ed68797bb36cb3eaab2943b8c30d28392365acb6
adityagitnandanwar <adityav.nandanwar@gmail.com>
2019-09-13 18:02:41 +0300

    newgit

---
+++ src/WF1/Androidgit.java
@@ -0,0 +1,30 @@
+package WF1;
+
+import org.testng.annotations.Test;
+
+public class Androidgit {
+
+	@Test
+
+	public void EighthTest()
+
+	{
+		System.out.print("8thTest");
+	}
+	
+	@Test
+
+	public void NinethTest()
+
+	{
+		System.out.print("9thTest");
+	}
+	
+	@Test
+
+	public void TenthTest()
+
+	{
+		System.out.print("10thTest");
+	}
+}
